3343b4457a68aa844cd49e801154397584a2f141
Shubham Pawar <shubham.d.pawar.3333@gmail.com>
2023-12-29 22:00:59 +0530

    Base Model Preparation Completed

---
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,27 @@
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from src.cnnClassifier import logger
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        preare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=preare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+        
+        
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> {STAGE_NAME} started <<<<<< ")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> {STAGE_NAME} completed <<<<<< ")
+    except Exception as e:
+        logger.exception(e)
+        raise e
